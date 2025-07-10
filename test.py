c9ea09d12995f5b71eb36e6692034b04a8daa7ef
Hex <1632648712@qq.com>
2025-07-10 10:44:18 +0800

    0.02 豆瓣爬取电影
    Signed-off-by: Hex <1632648712@qq.com>

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://movie.douban.com/top250?start='
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.97 Safari/537.36 Core/1.116.537.400 QQBrowser/19.4.6561.400'
 }
-url = 'https://movie.douban.com/top250?start='
-
-res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'lxml')
-
-item=soup.find_all('div', class_='item')
-
-for i in item:
-    rank=i.find('em').text
-    title=i.find('span', class_='title').text
-    info=i.find('p').text.strip().replace('\n', '').replace(' ', '')
-    rate=i.find('span', class_='rating_num').text
-    print(f"排名：{rank} 标题：{title} \t评分：{rate} \t简介：{info}")
-
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
+for item in soup.find_all('div', class_='item'):
+    rank = item.em.text
+    title = item.find('span', class_='title').text
+    info = item.find('div', class_='bd').p.text.strip().replace('\n', '').replace(' ', '')
+    rate = item.find('span', property='v:average').text
+    print(f'排名:{rank},标题:{title} , 简介:{info}, 评分:{rate}')
